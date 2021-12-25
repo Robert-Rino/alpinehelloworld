d1f6f44346c5fac499c0d2b147e56ed9e630a7fc
Nino Chang <nino.chang@swag.live>
2021-12-25 16:14:48 +0800

    WIP

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f'Hello world version {os.environ["version"]}'
+    return f'Hello aaa {os.environ["version"]}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
